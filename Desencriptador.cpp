3807967a2eb6434f7170b568fb8287d0fc014ae5
S3RviK <Pedrochu10120@gmail.com>
2025-11-27 18:21:09 -0700

    agrege cositas al codigo
    ya esta completo creo

--- Desencriptador.cpp
+++ Desencriptador.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
+
 int main() {
-int cantidad;
-cout << "Equipo: Pedro, Santiago y Diego" << endl;
-cout << endl;
-cout << "Bienvenido al decodificador de codificacion numerica invertida" << endl;
-cout << endl;
-cout << "Cuantos conjuntos de numeros tiene tu mensaje (ej: 111 222 son dos conjuntos): ";
-cin >> cantidad;
-vector<int> arreglo(cantidad);
-cout << "Ingresa los numeros separados por espacio (ej: 111 97 100): " << endl;
-for(int i = 0; i < cantidad; i++) {
-cin >> arreglo[i];
-}
-return 0;
+    int cantidad;
+
+    cout << "Equipo: Pedro, Santiago y Diego" << endl;
+    cout << endl;
+    cout << "Bienvenido al decodificador de codificacion numerica invertida" << endl;
+    cout << endl;
+    cout << "Cuantos conjuntos de numeros tiene tu mensaje (ej: 111 222 son dos conjuntos): ";
+    cin >> cantidad;
+
+    vector<int> arreglo(cantidad);
+
+    cout << "Ingresa los numeros separados por espacio (ej: 111 97 100): " << endl;
+    for(int i = 0; i < cantidad; i++) {
+        cin >> arreglo[i];
+    }
+
+    // Se invierten los extremos
+    for(int i = 0; i < cantidad / 2; i++) {
+        int auxiliar = arreglo[i];
+        arreglo[i] = arreglo[cantidad - 1 - i];
+        arreglo[cantidad - 1 - i] = auxiliar;
+    }
+
+    cout << "Arreglo ordenado correctamente: ";
+    for(int i = 0; i < cantidad; i++) {
+        cout << arreglo[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Mensaje decifrado: ";
+    // Se convierte a caracteres
+    for(int i = 0; i < cantidad; i++) {
+        cout << char(arreglo[i]);
+    }
+    cout << endl << endl;
+
+    return 0;
 }
